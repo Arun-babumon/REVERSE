ea02afacbeff59f8e5e08fbbdd57796b3aa65c2e
arun.babumon <arun.babumon@ARUNBABUMON>
2023-02-14 16:18:00 +0530

    Add project files.

---
+++ REVERSE/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace REVERSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[20];
+            Console.WriteLine("Enter the Name");
+            
+            a= Console.ReadLine();
+            var b = a.Reverse();
+            Console.WriteLine("reversed name is"+ b );
+
+                }
+    }
+}
